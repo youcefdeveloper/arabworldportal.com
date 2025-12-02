aff069d481818242691da2695a9a471d4a6b6999
Youcef <youcef.developer@gmail.com>
2025-12-02 17:35:14 -0500

    added analytics

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,21 +1,21 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCZvAwp3xPcjBI_4FAvUfNKEkVPpn09PYU",
-//   authDomain: "kebportal.firebaseapp.com",
-//   projectId: "kebportal",
-//   storageBucket: "kebportal.firebasestorage.app",
-//   messagingSenderId: "282019521086",
-//   appId: "1:282019521086:web:64f7a1dd6bc33fdab0276f",
-//   measurementId: "G-GMT3T2RVWD"
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCW8EJ2CHrc7a-oD4pwsHAuZtSOC023dgk",
+  authDomain: "arabworldportal.firebaseapp.com",
+  projectId: "arabworldportal",
+  storageBucket: "arabworldportal.firebasestorage.app",
+  messagingSenderId: "625980565930",
+  appId: "1:625980565930:web:f3c5eadfc21bb7021763d3",
+  measurementId: "G-3G5T078L7M"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
